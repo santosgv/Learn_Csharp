fbc9d91035c6491f6516d2b1eb164f802d6a5fde
Vitor Gomes <vitorgomes@patrus.com.br>
2020-09-20 19:38:39 -0300

    Exemplos

---
+++ Myapp/Myapp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // realizando calculo apartir de inputs
+            //var ano =Convert.ToInt32("2020");
+            //var idade = Console.ReadLine();
+            //int resultado = ano - (Convert.ToInt32(idade));
+            //Console.WriteLine("Voce nasceu em : "+resultado);
+
+            // Criando arreys
+            //string[] Nomes = new String[] { "Vitor", "Santos", "Gomes" };
+
+            //List<String> Nomes = new List<String>();
+
+            //Nomes.Add("Vitor");
+            //Nomes.Add("Vivian");
+            //Nomes.Add("Vitoria");
+            //Console.WriteLine(" Posicao iniciao " + Nomes[0] +" de um tamanho de "+Nomes.Count+" a Lista");
+
+
+        }
+    }
+}
