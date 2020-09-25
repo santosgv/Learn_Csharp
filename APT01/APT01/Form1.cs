455e352a097a92e26b394fd68aaae6dae36149d9
Vitor Gomes <vitorgomes@patrus.com.br>
2020-09-25 12:11:33 -0300

    app

---
+++ APT01/APT01/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APT01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+  
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = num1 + num2;
+
+            label4.Text = Convert.ToString(resultado);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = num1 - num2;
+
+            label4.Text = Convert.ToString(resultado);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = num1 * num2;
+
+            label4.Text = Convert.ToString(resultado);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = num1 / num2;
+
+            label4.Text = Convert.ToString(resultado);
+        }
+    }
+}
