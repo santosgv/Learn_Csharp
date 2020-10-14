3263a71a72d0efc9b56c6a9d9c95a3c248fa3b5c
d4rk3n3ss <54445515+d4rk3n3ss@users.noreply.github.com>
2020-10-13 22:31:21 -0300

    a

---
+++ learn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Cachorro
+{
+	public Cachorro()
+	{
+		string nome;
+		string raca;
+	}
+	public faz();
+	{
+		Console.Writeline("Latir");
+		}
+}
