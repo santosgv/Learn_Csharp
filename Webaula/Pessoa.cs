fcf16ddbe1a2cd9223f1d281fe8bcfa52b73c10a
d4rk3n3ss <54445515+d4rk3n3ss@users.noreply.github.com>
2020-10-29 21:54:47 -0300

    classe

---
+++ Webaula/Pessoa.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webaula
+{
+    class _Pessoa
+    {
+        private DateTime _DataCadastro;
+        private int _Codigo;
+        private string _Nome;
+        private string _Cpf;
+        private string _Endereco;
+        private string _Cidade;
+        private string _Uf;
+        private string _Cep;
+        public DateTime DataCadastro
+        {
+            get { return _DataCadastro; }
+            set { _DataCadastro = value; }
+        }
+
+        public int Codigo
+        {
+            get { return _Codigo; }
+            set { _Codigo = value; }
+        }
+
+        public string Nome
+        {
+            get { return _Nome; }
+            set { _Nome = value; }
+        }
+
+        public string Cpf
+        {
+            get { return _Cpf; }
+            set { _Cpf = value; }
+        }
+
+        public string Endereco
+        {
+            get { return _Endereco; }
+            set { _Endereco = value; }
+        }
+
+        public string Cidade
+        {
+            get { return _Cidade; }
+            set { _Cidade = value; }
+        }
+
+        public string Uf
+        {
+            get { return _Uf; }
+            set { _Uf = value; }
+        }
+
+        public string Cep
+        {
+            get { return _Cep; }
+            set { _Cep = value; }
+        }
+    }
+
+}
---
+++ Webaula/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Webaula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Pessoa Pessoa = new _Pessoa();
+            Console.WriteLine("Digite o codigo");
+
+            Pessoa.Codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o nome.......:");
+            Pessoa.Nome = Console.ReadLine();
+
+            Console.Write("Digite o cpf........:");
+            Pessoa.Cpf = Console.ReadLine();
+
+            Console.Write("Digite o endereço...:");
+            Pessoa.Endereco = Console.ReadLine();
+
+            Console.Write("Digite a cidade.....:");
+            Pessoa.Cidade = Console.ReadLine();
+
+            Console.Write("Digite o estado.....:");
+            Pessoa.Uf = Console.ReadLine();
+
+            Console.Write("Digite o cep........:");
+            Pessoa.Cep = Console.ReadLine();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("   Mostrando o conteúdo da classe Pessoa");
+            Console.WriteLine("=========================================");
+
+            Console.WriteLine($"Codigo.......: {Pessoa.Codigo}");
+            Console.WriteLine($"Nome.........: {Pessoa.Nome}");
+            Console.WriteLine($"Cpf..........: {Pessoa.Cpf}");
+            Console.WriteLine($"Edereço......: {Pessoa.Endereco}");
+            Console.WriteLine($"Cidade.......: {Pessoa.Cidade}");
+            Console.WriteLine($"Estado.......: {Pessoa.Uf}");
+            Console.WriteLine($"Cep..........: {Pessoa.Cep}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
